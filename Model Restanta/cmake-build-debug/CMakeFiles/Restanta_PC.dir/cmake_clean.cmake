092a5a41fd72a6ea0070852079d9439ddefa19b5
Laurentiu Vasile Cretu <37586514+laurentiucretu68@users.noreply.github.com>
2021-09-20 21:07:21 +0300

    Add files via upload

---
+++ Model Restanta/cmake-build-debug/CMakeFiles/Restanta_PC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Restanta_PC.dir/main.c.obj"
+  "Restanta_PC.exe"
+  "Restanta_PC.exe.manifest"
+  "Restanta_PC.pdb"
+  "libRestanta_PC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Restanta_PC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
